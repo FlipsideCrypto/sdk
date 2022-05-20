3a7db41e78e6aa198cde3066026ede0713c7a1bf
Jim Myers <jfmyers01@gmail.com>
2022-05-19 21:47:31 -0400

    Add comments

--- js/src/types/query.type.ts
+++ js/src/types/query.type.ts
@@ -1,7 +1,10 @@
 export type Query = {
+  // SQL query to execute
   sql: string;
+  // The number of minutes to cache the query results
   ttlMinutes?: number;
+  // An override on the cahce. A value of true will reexecute the query.
   cached?: boolean;
+  // The number of minutes until your query time out
   timeoutMinutes?: number;
-  retryIntervalSeconds?: number;
 };

9832325e3b27133b16a2921336b66faf18db99ba
Jim Myers <jfmyers01@gmail.com>
2022-07-20 15:29:01 -0400

    Style updates

--- python/shroomdk/utils/sleep.py
+++ python/shroomdk/utils/sleep.py
@@ -2,8 +2,10 @@ import time
 
 from shroomdk.models.sleep_config import SleepConfig
 
+
 def sec_to_ms(sec: int):
     return sec * 1000
+
 
 def get_exp_backoff_seconds(attempts: int | float):
     return 2**attempts
@@ -59,89 +61,8 @@ def linear_backoff(config: SleepConfig):
     if elapsed_seconds / 60 > config.timeout_minutes:
         should_continue = False
         return should_continue
-    print("sleeping for {} seconds".format(get_linear_backoff_seconds(config.attempts, config.interval_seconds)))
+
+    # print("sleeping for {} seconds".format(get_linear_backoff_seconds(config.attempts, config.interval_seconds)))
     time.sleep(get_linear_backoff_seconds(config.attempts, config.interval_seconds))
-    
+
     return should_continue
-
-
-# import { SleepConfig } from "../types/sleep-config.type";
-# import { secToMs } from "./time";
-
-# export async function sleep(ms: number): Promise<undefined> {
-#   return new Promise((resolve) => setTimeout(resolve, ms));
-# }
-
-# function getExpBackOffSeconds(attempts: number) {
-#   return Math.pow(2, attempts);
-# }
-
-# function getLinearBackOffSeconds(attempts: number, intervalSeconds: number) {
-#   return (attempts + 1) * intervalSeconds;
-# }
-
-# export function getElapsedExpSeconds(config: SleepConfig) {
-#   if (!config.intervalSeconds) {
-#     throw new Error("intervalSeconds is required for getElapsedLinearTime");
-#   }
-#   let elapsedSeconds = 0;
-#   for (let i = 0; i < config.attempts; i++) {
-#     elapsedSeconds += getExpBackOffSeconds(i);
-#   }
-#   return elapsedSeconds;
-# }
-
-# export async function expBackOff(config: SleepConfig): Promise<boolean> {
-#   if (!config.intervalSeconds) {
-#     throw new Error("intervalSeconds is required for getElapsedLinearTime");
-#   }
-
-#   let elapsedSeconds = getElapsedExpSeconds(config);
-
-#   let shouldContinueBackoff = true;
-#   if (elapsedSeconds / 60 > config.timeoutMinutes) {
-#     shouldContinueBackoff = false;
-#     return shouldContinueBackoff;
-#   }
-
-#   const msToSleep = secToMs(
-#     getExpBackOffSeconds(config.attempts * config.intervalSeconds)
-#   );
-
-#   await sleep(msToSleep);
-
-#   return shouldContinueBackoff;
-# }
-
-# export function getElapsedLinearSeconds(config: SleepConfig) {
-#   if (!config.intervalSeconds) {
-#     throw new Error("intervalSeconds is required for getElapsedLinearTime");
-#   }
-#   let elapsedSeconds = 0;
-#   for (let i = 0; i < config.attempts; i++) {
-#     elapsedSeconds += getLinearBackOffSeconds(i, config.intervalSeconds);
-#   }
-#   return elapsedSeconds;
-# }
-
-# export async function linearBackOff(config: SleepConfig): Promise<boolean> {
-#   if (!config.intervalSeconds) {
-#     throw new Error("intervalSeconds is required for linearBackOff");
-#   }
-
-#   let elapsedSeconds = getElapsedLinearSeconds(config);
-
-#   let shouldContinueBackoff = true;
-#   if (elapsedSeconds / 60 > config.timeoutMinutes) {
-#     shouldContinueBackoff = false;
-#     return shouldContinueBackoff;
-#   }
-
-#   const msToSleep = secToMs(
-#     getLinearBackOffSeconds(config.attempts, config.intervalSeconds)
-#   );
-
-#   await sleep(msToSleep);
-
-#   return shouldContinueBackoff;
-# }

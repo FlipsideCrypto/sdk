c2295c4ddfd778a95d1635e730aa2436675828b0
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2023-05-08 10:11:23 -0400

    Update create_query_token.R
    Conforms to RPC Format for generating a query token

--- r/shroomDK/R/create_query_token.R
+++ r/shroomDK/R/create_query_token.R
@@ -9,8 +9,9 @@ library(httr)
 #'
 #' @param query Flipside Crypto Snowflake SQL compatible query as a string.
 #' @param api_key Flipside Crypto ShroomDK API Key
-#' @param ttl time (in minutes) to keep query in cache.
-#' @param cache Use cached results; set as FALSE to re-execute.
+#' @param ttl time-to-live (in hours) to keep query results available. Default 1 hour.
+#' @param mam max-age-minutes, lifespan of cache. set to 0 to always re-execute. Default 10 minutes.
+#' @param api_url default to https://api-v2.flipsidecrypto.xyz/json-rpc but upgradeable for user.
 #' @return list of `token` and `cached` use `token` in `get_query_from_token()`
 #' @import jsonlite httr
 #' @export
@@ -20,13 +21,15 @@ library(httr)
 #' create_query_token(
 #' query = "SELECT * FROM ethereum.core.fact_transactions LIMIT 1",
 #' api_key = readLines("api_key.txt"),
-#' ttl = 15,
-#' cache = TRUE)
+#' ttl = 1,
+#' mam = 5)
 #'}
-create_query_token <- function(query, api_key, ttl = 10, cache = TRUE){
+create_query_token <- function(query, api_key,
+                               ttl = 1,
+                               mam = 10,
+                               api_url = "https://api-v2.flipsidecrypto.xyz/json-rpc"){
 
   headers = c(
-    "Accept" = 'application/json',
     "Content-Type" = 'application/json',
     "x-api-key" = api_key
   )
@@ -42,18 +45,39 @@ create_query_token <- function(query, api_key, ttl = 10, cache = TRUE){
             results over time and not be reproducible. They may also be harder to paginate.")
   }
 
+  # this exact structure including pretty is required
+  # new RPC API cannot take direct JSON
+ request_body <- as.character(
+    jsonlite::toJSON(pretty = TRUE,
+                     list(
+                       "jsonrpc" = "2.0",
+                       "method" = "createQueryRun",
+                       "params" = list(
+                         list(         "resultTTLHours" = ttl,
+                                       "maxAgeMinutes" = mam,
+                                       "sql" = query,
+                                       "tags" = list(
+                                         "sdk_package" = "R",
+                                         "sdk_version" = "0.2.0",
+                                         "sdk_language" = "R"
+                                       ),
+                                       "dataSource" = "snowflake-default",
+                                       "dataProvider" = "flipside"
+                         )
+                       ),
+                       "id" = 1
+                     ),
+                     auto_unbox = TRUE
+    )
+  )
+
+
+
   # must use auto_unbox to ensure toJSON does not wrap string in an array
   res <- httr::POST(
-    "https://node-api.flipsidecrypto.com/queries",
+    api_url,
     config = httr::add_headers(.headers = headers),
-    body = jsonlite::toJSON(
-      list("sql" = query,
-           "ttlMinutes" = ttl,
-           "cache" = cache,
-           "sdk_package" = "R",
-           "sdk_version" = "0.1.1"),
-      auto_unbox = TRUE)
-  )
+    body = request_body)
 
   token <- content(res)
 

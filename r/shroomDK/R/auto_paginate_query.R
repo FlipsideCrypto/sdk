bb944b1c19311eaf972672b51b769e80999383e1
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2023-05-08 14:09:55 -0400

    Known issue on 10th page for 10,000 row queries ??

--- r/shroomDK/R/auto_paginate_query.R
+++ r/shroomDK/R/auto_paginate_query.R
@@ -1,47 +1,65 @@
+library(jsonlite)
+library(httr)
 
 #' Auto Paginate Queries
 #'
-#' @description Grabs up to maxrows in a query by going through each page 100k rows at a time.
+#' @description Grabs up to maxrows in a query by going through each page to download one at a time.
 #'
 #' @param query The SQL query to pass to ShroomDK
 #' @param api_key ShroomDK API key.
-#' @param maxrows Max rows allowed in ShroomDK, 1M at time of writing.
-#'
-#' @return data frame of up to 1M rows, see ?clean_query for more details on column classes.
+#' @param page_size Default 1000. May return error if page_size is tool large and data to exceed 30MB.
+#' @param page_count Default 1. How many pages, of page_size rows each, to read.
+#' @param api_url default to https://api-v2.flipsidecrypto.xyz/json-rpc but upgradeable for user.
+#' @return data frame of up to `page_size * page_count` rows, see ?clean_query for more details on column classes.
+#' @import jsonlite httr
 #' @export
 #' @examples
 #' \dontrun{
 #' pull_data <- auto_paginate_query("
 #' SELECT * FROM ETHEREUM.CORE.FACT_TRANSACTIONS LIMIT 10000",
-#' api_key = readLines("api_key.txt"))
+#' api_key = readLines("api_key.txt"),
+#' page_count = 10)
 #' }
-auto_paginate_query <- function(query, api_key, maxrows = 1000000){
+auto_paginate_query <- function(query, api_key, page_size = 1000,
+                                page_count = 1,
+                                api_url = "https://api-v2.flipsidecrypto.xyz/json-rpc"){
+browser()
+  qtoken <- create_query_token(query = query,
+                               api_key = api_key,
+                               ttl = 1,
+                               mam = 10,
+                               api_url = api_url)
 
-  qtoken <- shroomDK::create_query_token(query = query, api_key = api_key)
-  res <- shroomDK::get_query_from_token(qtoken$token, api_key = api_key)
- df <- shroomDK::clean_query(res)
+  # read the first page
+  res <- get_query_from_token(qtoken$result$queryRequest$queryRunId,
+                                        api_key = api_key,
+                                        page_number = 1,
+                                        page_size = page_size,
+                                        result_format = "csv",
+                                        api_url = api_url)
+  df <- clean_query(res)
 
   # Handle Pagination via ShroomDK
-  # up to 1M rows max
-  # get 100,000 rows at a time
-  # stop when the most recent page < 100,000 items.
-  # otherwise stop at 1M total rows.
-  # NOTE: in the future, if we allow > 1M rows, will need to update this.
-
- maxpages = ceiling(maxrows/100000)
-
-  if(nrow(df) == 100000){
+  # if you got a full page immediately, keep going
+  if(nrow(df) == page_size){
     warning("Checking for additional pages of data...")
-    for(i in 2:maxpages){
-      temp_page <- clean_query(
-        shroomDK::get_query_from_token(qtoken$token,
+    for(i in 2:page_count){
+      temp_page <- get_query_from_token(qtoken$result$queryRequest$queryRunId,
                                        api_key = api_key,
-                                       page_number = i)
-      )
+                                       page_number = i,
+                                       page_size = page_size,
+                                       result_format = "csv",
+                                       api_url = api_url)
 
-     df <- rbind.data.frame(df, temp_page)
+      if(length(temp_page$result$rows) > 0){
+        temp_page <- clean_query(temp_page)
+      } else {
+        temp_page <- data.frame()
+      }
 
-      if(nrow(temp_page) < 100000 | i == maxpages){
+      df <- rbind.data.frame(df, temp_page)
+
+      if(nrow(temp_page) < page_size | i == page_count){
         # done
         return(df)
 
@@ -52,6 +70,4 @@ auto_paginate_query <- function(query, api_key, maxrows = 1000000){
   } else {
     return(df)
   }
-
-
-  }
+}
